1793891ccf19645fdebb08914e0003b8011f9b82
PanoramaView <zhou-1997@hotmail.it>
2021-05-09 09:05:11 +0200

    progress v-for

--- myscripts.js
+++ myscripts.js
@@ -2,13 +2,18 @@ const app = new Vue({
     el: "#app",
     data: {
         usersList: globalUsersList,
+        selectedUser: 0,
+        indexUser: 0
     },
     comoputed:{
 
     },
     methods:{
-        getPropicPath(propic){
-            return
+        getUserPropic(avatar){
+            return ``
+        },
+        onUserClick(user){
+            index= user
         }
     }
 })
